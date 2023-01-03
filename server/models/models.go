1ba50d810284f4bbfc77683d95239f1fee8a69e4
Felice <felicepng2010@gmail.com>
2023-01-03 23:42:46 +0800

    feat: update prompt generation

--- server/models/models.go
+++ server/models/models.go
@@ -1,5 +1,5 @@
 package models
 
 type MoodboardJson struct {
-	Theme string `json:"theme"`
+	Theme *string `json:"theme"`
 }
--- server/openai/openai.go
+++ server/openai/openai.go
@@ -1,13 +1,10 @@
 package openai
 
 import (
-	"bytes"
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/felicepng/moodboard/models"
 	"github.com/gin-gonic/gin"
@@ -16,23 +13,32 @@ import (
 
 func GenerateImages(c *gin.Context) {
 	var moodboard models.MoodboardJson
-	if err := c.BindJSON(&moodboard); err != nil {
-		log.Println("Error occurred unmarshalling moodboard theme:", err)
+	if err := json.NewDecoder(c.Request.Body).Decode(&moodboard); err != nil {
+		log.Printf("Error occurred unmarshalling json: %v\n", err)
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Error occurred unmarshalling moodboard theme",
+			"message": "Error occurred unmarshalling json",
 		})
 		return
 	}
 
-	GeneratePromptsFromTheme(moodboard.Theme)
+	prompts, err := GeneratePromptsFromTheme(moodboard.Theme)
+	if err != nil {
+		log.Printf("Error occurred generating prompts: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error occurred generating prompts",
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Images generated",
+		"message": prompts,
 	})
 }
 
-func GeneratePromptsFromTheme(theme string) string {
-	log.Println("Theme:", theme)
+func GeneratePromptsFromTheme(theme *string) (string, error) {
+	if theme != nil {
+		log.Printf("Theme: %s", *theme)
+	}
 
 	err := godotenv.Load()
 	if err != nil {
@@ -40,38 +46,40 @@ func GeneratePromptsFromTheme(theme string) string {
 	}
 
 	apiKey := os.Getenv("API_KEY")
-	log.Println("API Key:", apiKey)
+	log.Printf("API Key: %s", apiKey)
 
-	data := map[string]interface{}{
-		"model":  "text-davinci-003",
-		"prompt": "Write 8 individual image prompts, numbered from 1 to 8, for a moodboard with the following theme: " + theme,
-		"n":      1,
-	}
+	// data := map[string]interface{}{
+	// 	"model":  "text-davinci-003",
+	// 	"prompt": "Write 8 individual image prompts, numbered from 1 to 8, for a moodboard with the following theme: " + theme,
+	// 	"n":      1,
+	// }
 
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// jsonData, err := json.Marshal(data)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	req, err := http.NewRequest(http.MethodPost, "https://api.openai.com/v1/completions", bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Add("Authorization", "Bearer "+apiKey)
+	// req, err := http.NewRequest(http.MethodPost, "https://api.openai.com/v1/completions", bytes.NewBuffer(jsonData))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// req.Header.Add("Authorization", "Bearer "+apiKey)
 
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Error:", err.Error())
-		return ""
-	}
-	defer resp.Body.Close()
+	// client := &http.Client{
+	// 	Timeout: time.Second * 10,
+	// }
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	log.Println("Error:", err.Error())
+	// 	return "", err
+	// }
+	// defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("Error while reading the response bytes:", err)
-	}
-	return string([]byte(body))
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Println("Error while reading the response bytes:", err)
+	// }
+	// return string([]byte(body)), nil
+
+	return "", nil
 }
