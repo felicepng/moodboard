8ac425564b2214790ffb918dc3bebead35a7ef3b
Felice <felicepng2010@gmail.com>
2023-01-03 10:26:47 +0800

    chore: initialise repo

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="h-screen w-screen flex justify-center items-center bg-gray-900 text-gray-300">
+      Moodboard
+    </div>
+  );
+};
+
+export default App;
